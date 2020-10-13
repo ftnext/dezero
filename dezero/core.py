dedea6af2eb8b52d35089b5eaa13527c19448d09
ftnext <takuyafjp+develop@gmail.com>
2020-10-13 23:23:48 +0900

    [refactor] step 2: refactor Function to base class and add concrete Square function

--- dezero/core.py
+++ dezero/core.py
@@ -20,10 +20,30 @@ class Variable:
 
 
 class Function:
-    """ある変数から別の変数への対応関係を定めるものである、関数を表すクラス
+    """ある変数から別の変数への対応関係を定めるものである、関数の基底クラス
+
+    すべての関数に共通する機能を実装している
+    """
+
+    def __call__(self, input: Variable) -> Variable:
+        x = input.data  # actual data
+        y = self.forward(x)
+        output = Variable(y)
+        return output
+
+    def forward(self, x):
+        """具体的な計算
+
+        Functionクラスを継承した具体的な関数クラスで定義する
+        """
+        raise NotImplementedError
+
+
+class Square(Function):
+    """入力された値を2乗する、具体的な関数
 
     >>> x = Variable(np.array(10))
-    >>> f = Function()
+    >>> f = Square()
     >>> y = f(x)
     >>> print(type(y))
     <class 'core.Variable'>
@@ -31,8 +51,5 @@ class Function:
     100
     """
 
-    def __call__(self, input: Variable) -> Variable:
-        x = input.data  # actual data
-        y = x ** 2
-        output = Variable(y)
-        return output
+    def forward(self, x):
+        return x ** 2

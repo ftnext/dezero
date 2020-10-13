07bebb1a2e75fbfc127ac88412da4645cca5d7bc
ftnext <takuyafjp+develop@gmail.com>
2020-10-13 23:08:10 +0900

    [feat] step 2: add Function class, but it's concrete

--- dezero/core.py
+++ dezero/core.py
@@ -1,9 +1,11 @@
+import numpy as np  # NOQA (for doctest)
+
+
 class Variable:
     """「箱」（データを持つ存在）としての変数を表すクラス
 
     data属性にデータが保持される
 
-    >>> import numpy as np
     >>> data = np.array(1.0)
     >>> x = Variable(data)
     >>> print(x.data)
@@ -15,3 +17,22 @@ class Variable:
 
     def __init__(self, data):
         self.data = data
+
+
+class Function:
+    """ある変数から別の変数への対応関係を定めるものである、関数を表すクラス
+
+    >>> x = Variable(np.array(10))
+    >>> f = Function()
+    >>> y = f(x)
+    >>> print(type(y))
+    <class 'core.Variable'>
+    >>> print(y.data)
+    100
+    """
+
+    def __call__(self, input: Variable) -> Variable:
+        x = input.data  # actual data
+        y = x ** 2
+        output = Variable(y)
+        return output
